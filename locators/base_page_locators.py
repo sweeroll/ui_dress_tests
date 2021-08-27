c8a679600b4dea7277fc5b75494a8f41ed70ddfb
kseniaglivko <k.glivko@mail.ru>
2021-08-27 19:23:38 +0300

    Fixed auth function.

--- locators/base_page_locators.py
+++ locators/base_page_locators.py
@@ -4,3 +4,4 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     SIGN_IN = (By.CLASS_NAME, "header_user_info")
     HEADER_USER_INFO = (By.CLASS_NAME, "header_user_info")
+    CONFIRM_EXIT_BUTTON = (By.XPATH, "//button[text()='Выход']")
