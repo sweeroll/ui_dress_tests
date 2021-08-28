ca5d281cfbf87ae01603ba310535a6cb35e93933
Rishat-F <nilluziaf@gmail.com>
2021-08-28 09:39:02 +0300

    Added constants for editing personal data

--- common/constants.py
+++ common/constants.py
@@ -1,2 +1,15 @@
 class LoginConstants:
     AUTH_ERROR = "Неверный логин или пароль, попробуйте заново."
+
+
+class PersonalDataConstants:
+    EMAIL_DISPLAY_MODES = ("0", "1", "2")
+    TIMEZONE_VALUES = (
+        "99",  # server's timezone
+        "Asia/Dubai",
+        "America/Santiago",
+        "Africa/Tunis",
+        "Europe/Moscow",
+        "Europe/Moscow",
+        "UTC",
+    )
